2c39530d6c575d9c8c08c48465b5b7fb161f072f
Wilkins Cheng <wilkins.cheng@gmail.com>
2020-06-16 18:29:08 +1000

    taskbar added toggle function and randomize

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 
 // API KEYS
 
+// Calculate Time
+
 let current_time = new Date();
 let dd = String(current_time.getDate());
 let mm = String(current_time.getMonth() + 1);
@@ -14,6 +16,8 @@ let loc = "Sydney";
 console.log(`Date: ${mm}/${dd}/${yy}`);
 console.log(`The time is ${hh}:${min}`);
 
+// Get Weather Data
+
 async function DisplayWeatherData() {
   let weather_data = await axios.get(
     `http://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${WEATHER_API_KEY}`
@@ -21,7 +25,7 @@ async function DisplayWeatherData() {
   let weather_forecast = await axios.get(
     `http://api.openweathermap.org/data/2.5/forecast?q=${loc}&appid=${WEATHER_API_KEY}`
   );
-  // console.log(weather_data);
+  console.log(weather_data);
   let sunrise = new Date(weather_data.data.sys.sunrise * 1000);
   let sunset = new Date(weather_data.data.sys.sunset * 1000);
   console.log(
@@ -38,6 +42,7 @@ async function DisplayWeatherData() {
     ).toFixed(2)}C`
   );
   console.log(`The five day forcast per day is as follow:`);
+  console.log(weather_forecast);
   let fiveday_forcast_temp = [];
   let fiveday_forcast_condition = [];
   for (let i = 0; i < 40; i += 8) {
@@ -51,7 +56,117 @@ async function DisplayWeatherData() {
 
   console.log(fiveday_forcast_condition);
   console.log(fiveday_forcast_temp);
+
+  // ICONS
+  // http://openweathermap.org/img/w/${icon}.png
 }
+
+// Calculate ToDo randomize data
+
+// Generate random array object
+
+let tasksobj = {
+  important: [
+    { id: "AA" },
+    { id: "AB" },
+    { id: "AC" },
+    { id: "AD" },
+    { id: "AE" },
+  ],
+  medium: [{ id: "BA" }, { id: "BB" }, { id: "BC" }, { id: "BD" }],
+  low: [
+    { id: "CA" },
+    { id: "CB" },
+    { id: "CC" },
+    { id: "CD" },
+    { id: "CE" },
+    { id: "CF" },
+  ],
+};
+
+//Extract to arrays
+let tsk1 = tasksobj.important.map((e) => {
+  return e.id;
+});
+
+let tsk2 = tasksobj.medium.map((e) => {
+  return e.id;
+});
+
+let tsk3 = tasksobj.low.map((e) => {
+  return e.id;
+});
+
+// Shuffle Array
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+tsk1 = shuffle(tsk1);
+tsk2 = shuffle(tsk2);
+tsk3 = shuffle(tsk3);
+
+console.log({ tsk1, tsk2, tsk3 });
+console.log(tsk1.length + tsk2.length + tsk3.length);
+
+const important = document.querySelector("#Important");
+const medium = document.querySelector("#Medium");
+const low = document.querySelector("#Low");
+
+tsk1.forEach((e, i) => {
+  let element = document.createElement("div");
+  element.classList.add("btn", "btn-primary", "col-2", "mx-auto");
+  element.id = `togglei-btn-${i}`;
+  let newContent = document.createTextNode(e);
+  element.appendChild(newContent);
+  important.appendChild(element);
+});
+
+tsk2.forEach((e, i) => {
+  let element = document.createElement("div");
+  element.classList.add("btn", "btn-primary", "col-2", "mx-auto");
+  element.id = `togglem-btn-${i}`;
+  let newContent = document.createTextNode(e);
+  element.appendChild(newContent);
+  medium.appendChild(element);
+});
+tsk3.forEach((e, i) => {
+  let element = document.createElement("div");
+  element.classList.add("btn", "btn-primary", "col-2", "mx-auto");
+  element.id = `togglel-btn-${i}`;
+  let newContent = document.createTextNode(e);
+  element.appendChild(newContent);
+  low.appendChild(element);
+});
+
+const variablearri = [];
+const variablearrm = [];
+const variablearrl = [];
+
+for (let i = 0; i < 5; i++) {
+  variablearri.push(document.querySelector(`#togglei-btn-${i}`));
+}
+
+for (let i = 0; i < 4; i++) {
+  variablearrm.push(document.querySelector(`#togglem-btn-${i}`));
+}
+
+for (let i = 0; i < 6; i++) {
+  variablearrl.push(document.querySelector(`#togglel-btn-${i}`));
+}
+
+const variablearr = [...variablearri, ...variablearrl, ...variablearrm];
+variablearr.forEach((e) => {
+  e.addEventListener("click", () => {
+    e.classList.toggle("btn-success");
+  });
+});
+
+// Get Stock Data
 
 async function DisplayUSStockData() {
   let stocks_interest = ["TSLA", "AAPL"];
@@ -70,3 +185,5 @@ async function DisplayUSStockData() {
 }
 DisplayWeatherData();
 DisplayUSStockData();
+
+// Get Jokes
